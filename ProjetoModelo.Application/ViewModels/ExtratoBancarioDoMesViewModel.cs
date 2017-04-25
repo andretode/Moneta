015d153495b4c630cc52972c4811c3a22d97f35d
Andre Ribeiro Barros <andre.tode@gmail.com>
2017-04-25 17:07:40 -0300

    Correção de bug ao alterar a data
    Correção de bug ao alterar a data

--- ProjetoModelo.Application/ViewModels/ExtratoBancarioDoMesViewModel.cs
+++ ProjetoModelo.Application/ViewModels/ExtratoBancarioDoMesViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,13 @@ namespace Moneta.Application.ViewModels
             get { return contaIdFiltro; }
             set { contaIdFiltro = value; }
         }
+
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yy}")]
+        [DataType(DataType.DateTime)]
+        [DisplayName("Mês/Ano")]
         public DateTime MesAnoCompetencia { get; set; }
+
         public virtual IEnumerable<ExtratoBancarioViewModel> ExtratosDoMes { get; set; }
     }
 }
