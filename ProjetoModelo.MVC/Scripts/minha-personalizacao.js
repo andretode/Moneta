217ca1c9e4d6ef93454cd9cb895f9b3280fa228a
Andre Ribeiro Barros <andre.tode@gmail.com>
2017-05-12 16:08:51 -0300

    Correção caracteres estranhos

--- ProjetoModelo.MVC/Scripts/minha-personalizacao.js
+++ ProjetoModelo.MVC/Scripts/minha-personalizacao.js
@@ -1,12 +1,12 @@
 $(function () { // will trigger when the document is ready
 	$(".datepicker").datepicker({
 		dateFormat: 'dd/mm/yy',
-		dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+		dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'S&aacute;bado'],
 		dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
 		dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
-		monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+		monthNames: ['Janeiro', 'Fevereiro', 'Mar&ccedil;o', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
 		monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-		nextText: 'Próximo',
+		nextText: 'Pr&oacute;ximo',
 		prevText: 'Anterior'
 	});
 });
